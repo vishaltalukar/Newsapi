32b26f66463861c42167525da482a6e9ad0a6de8
Vishal Talukar <64702006+vishaltalukar@users.noreply.github.com>
2021-02-14 17:31:22 +0530

    Add files via upload

---
+++ invest.php
@@ -0,0 +1,138 @@
+<!-- CSS only -->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Investing</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <style>
+ .container{
+    margin-top: 20px;
+    font-family: "Lucida Console", "Courier New", monospace;
+  }
+
+.title{
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-right: 20px;
+  margin-left: 20px;
+  font-family: "Lucida Console", "Courier New", monospace;
+}
+.single-news{
+  background-color:  #e6f9ff;
+  padding: 35px;
+  margin-bottom: 20px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-right: 30px;
+  margin-left: 30px;
+  font-family: "Lucida Console", "Courier New", monospace;
+
+}
+.single-news:hover
+{
+font-family: "Times New Roman", Times, serif;
+color: #456745;
+
+
+</style>
+
+</head>
+<body>
+
+<header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+<a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="landing.php">      
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">Newsopedia | Investing</span>
+    </a>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900" href="tech.php">Tech </a>
+      <a class="mr-5 hover:text-gray-900" href="fin.php">Finance </a>
+      <a class="mr-5 hover:text-gray-900" href="poly.php">Politics</a>
+      <a class="mr-5 hover:text-gray-900" href="edu.php">Education</a>
+    </nav>
+    
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    
+  </div><hr>
+</header>
+
+
+      
+        <div class="list-wrapper">
+      <?php
+      
+        $api_url='https://newsapi.org/v2/everything?q="Investing"&apiKey=5e057beb78d34ec0b5d8399b03494c19';
+      
+        $newslist=json_decode(file_get_contents($api_url));
+        foreach ($newslist->articles as $news){
+        ?> 
+      
+
+        <div class="row single-news">
+        <div class="col-4">
+         <img style="width: 100%" src="<?php echo $news->urlToImage  ?>">
+        </div>
+      <div class="col-8">
+        <h2><?php echo $news->title; ?></h2>
+        <small>
+
+          <?php echo $news->source->name;  ?> | author - 
+          <?php echo $news->author; ?>
+        </small>
+        <p>
+          <?php echo $news->description;  ?>
+       
+          </p>
+        <a href="<?php echo $news->url ?>" style="float: right;" target="_blank">Read More....</a>
+            </div>
+        </div>
+        
+        <?php } ?>
+    </div>      
+      
+
+
+
+<footer class="text-gray-600 body-font">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      
+      <span class="ml-3 text-xl">Newsopedia</span>
+    </a>
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"> 2021 Newsopedia —
+      <a href="https://github.com/vishaltalukar" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@vishaltalukar </a>
+    </p>
+    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+
+      <a class="ml-3 text-gray-500" href="https://twitter.com/VishalTalukar">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500" href="https://www.instagram.com/vishal.talukar66/">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500" href="https://www.linkedin.com/in/vishal-talukar-6a5682177/">
+        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+        </svg>
+      </a>
+    </span>
+  </div>
+</footer>
+
+
+
+
+
+</body>
+</html>
---
+++ landing.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+	<title>Newsopedia</title>
+</head>
+<body>
+
+
+
+<header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+<a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="landing.php">      
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">Newsopedia</span>
+    </a>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900" href="tech.php">Tech </a>
+      <a class="mr-5 hover:text-gray-900" href="fin.php">Finance </a>
+      <a class="mr-5 hover:text-gray-900" href="poly.php">Politics</a>
+      <a class="mr-5 hover:text-gray-900" href="edu.php">Education</a>
+    </nav>
+    
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    
+  </div><hr>
+</header>
+
+
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="tech.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://source.unsplash.com/420x260/?Tech">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium"> Tech </h2>
+          
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="fin.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://images.unsplash.com/photo-1518186233392-c232efbf2373?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=260&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=420">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium"> Finance </h2>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="poly.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://source.unsplash.com/420x260/?Politics">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium"> Politics</h2>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="edu.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://source.unsplash.com/420x260/?library">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium"> Education </h2>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="world.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://source.unsplash.com/420x260/?world">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium"> World </h2>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="sport.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://source.unsplash.com/420x260/?sport">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium">Sports </h2>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="stock.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://images.unsplash.com/photo-1565514158740-064f34bd6cfd?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=260&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=420">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium">Stock Market </h2>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden" href="invest.php">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://images.unsplash.com/photo-1611967556157-d5c8830b5161?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=260&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=420">
+        </a>
+        <div class="mt-4">
+          
+          <h2 class="text-gray-900 title-font text-lg font-medium">Investing</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="text-gray-600 body-font relative">
+  <div class="absolute inset-0 bg-gray-300">
+    <iframe style="filter: grayscale(0.5) contrast(1.1) opacity(0.5);" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3838.7851478728116!2d74.48727449869047!3d15.8152789475523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbf65c556d2f08f%3A0xcbbcbb73e1286392!2sKLS%20Gogte%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1613288929543!5m2!1sen!2sin" width="100%" height="100%" frameborder="0"></iframe>
+  </div>
+  <div class="container px-5 py-24 mx-auto flex">
+    <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+        <div class="lg:w-1/2 px-8">
+          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">Student At</h2>
+          <p class="mt-3">KLS Gogte Institute of Technology <br>"Jnana Ganga",<br>Udyambag, Belagavi 590008 <br> Karnataka, India</p>
+        </div>
+        <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
+          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+          <a class="text-blue-500 leading-relaxed"><a href="mailto:git20is400@students.git.edu">git20is400@students.git.edu</a></a>
+          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+          <p class="leading-relaxed">8867068372</p>
+        </div>
+      
+    </div>
+  </div>
+</section>
+
+
+ 
+
+
+<footer class="text-gray-600 body-font">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      
+      <span class="ml-3 text-xl">Newsopedia</span>
+    </a>
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"> 2021 Newsopedia —
+      <a href="https://github.com/vishaltalukar" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@vishaltalukar </a>
+    </p>
+    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+
+      <a class="ml-3 text-gray-500" href="https://twitter.com/VishalTalukar">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500" href="https://www.instagram.com/vishal.talukar66/">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500" href="https://www.linkedin.com/in/vishal-talukar-6a5682177/">
+        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+        </svg>
+      </a>
+    </span>
+  </div>
+</footer>
+
+
+
+
+</body>
+</html>
